71ded4ebef6ad1ac730062c22ec024b3c2bfc8b9
hogana3atwit <43997359+hogana3atwit@users.noreply.github.com>
2022-03-30 14:37:56 -0400

    Update FizzBuzz.java

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -2,16 +2,38 @@ package com.demo;
 
 public class FizzBuzz 
 {
-    String processNumber(int num)
-	{	
-    	// Copy your code here
-		return "";
-	}
-	
-	String processNumbers (int[] numList)
-	{
-    	// Copy your code here
-		return "";
-	}
+    /**
+	 * Process a number
+	 * @param num a number
+	 * @return a string corresponds to that number
+	 */
+	public static String processNumber(int num) {
+        // TODO: Write your code here
+        if (num < 0) return "";
+        if (num == 0) return "0";
+        if (num % 15 == 0) return "FizzBuzz";
+        if (num % 3 == 0) return "Fizz";
+        if (num % 5 == 0) return "Buzz";
+        return num+"";
+    }
+    
+    /**
+	 * Process numbers
+	 * @param nums an array of numbers
+	 * @return a string corresponds to that numbers array
+	 */
+	public static String processNumbers(int[] nums) {
+        // TODO: Write your code here
+        String proc_num, new_nums = "";
+        for (int i = 0; i < nums.length; i++) {
+          proc_num = processNumber(nums[i]);
+          if ((i == nums.length-1) || (proc_num == "")) {
+            new_nums = new_nums + proc_num;
+          } else {
+            new_nums = new_nums + proc_num + ", ";
+          }
+        }
+        return new_nums;
+    }
 
 }
